dec28b9fdd116e631b791abe1370cd02ea1ec2ad
Leonhardt Koepsell <hello@lnhrdt.com>
2024-11-09 10:15:11 -0600

    Add a next-repository-tag-number action

---
+++ next-repository-tag-number/get-next-tag-number.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+REPOSITORY_URL=$1
+BEARER_TOKEN=$2
+INITIAL_NUMBER=${3:-1}
+TAG_PREFIX=${4:-"build-"}
+
+if [ -n "$BEARER_TOKEN" ]; then
+  AUTH_HEADER="-H \"Authorization: Bearer ${BEARER_TOKEN}\""
+else
+  AUTH_HEADER=""
+fi
+
+TAGS=$(curl -s $AUTH_HEADER "${REPOSITORY_URL}/tags/list" | jq -r '.tags[]' || echo "")
+
+if [ -z "$TAGS" ]; then
+  echo "No existing tags were found. Using initial tag number ${INITIAL_NUMBER}."
+  NEXT_TAG_NUMBER=$INITIAL_NUMBER
+else
+  HIGHEST_TAG=$(echo "$TAGS" | grep -Eo "${TAG_PREFIX}[0-9]+" | sort -V | tail -n 1)
+
+  if [ -z "$HIGHEST_TAG" ]; then
+    echo "No existing tags with the specified prefix were found. Using initial tag number ${INITIAL_NUMBER}."
+    NEXT_TAG_NUMBER=$INITIAL_NUMBER
+  else
+    HIGHEST_TAG_NUMBER=$(echo "$HIGHEST_TAG" | grep -Eo '[0-9]+')
+    NEXT_TAG_NUMBER=$((HIGHEST_TAG_NUMBER + 1))
+  fi
+fi
+
+NEXT_TAG="${TAG_PREFIX}${NEXT_TAG_NUMBER}"
+echo "Next tag: $NEXT_TAG"
+echo "tag=$NEXT_TAG" >> $GITHUB_OUTPUT
+echo "number=$NEXT_TAG_NUMBER" >> $GITHUB_OUTPUT
